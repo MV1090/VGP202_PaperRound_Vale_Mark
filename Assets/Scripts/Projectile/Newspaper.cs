6ab29b68ff9259d2197d58e62e03f9b6389a85a2
Mark <mark.vale@torontofilmschool.ca>
2024-09-14 21:22:20 -0600

    Added scale increase to newspaper

--- Assets/Scripts/Projectile/Newspaper.cs
+++ Assets/Scripts/Projectile/Newspaper.cs
@@ -1,40 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Newspaper : Projectile
 {
     Vector2 targetPos;
-    private Vector2 throwVector = Vector2.zero;
-    private Vector2 startingPos = Vector2.zero;
+    Vector2 targetDir;
+    Vector2 throwVector = Vector2.zero;
+    Vector2 startingPos = Vector2.zero;
+    Vector2 distanceVector;
+    Vector2 currentPos;
 
     [SerializeField] float speed;
     [SerializeField] float rotationSpeed;
-    [SerializeField] float rotationAmount;
-    
-
+    [SerializeField] float rotationAmount;    
     [SerializeField] AudioClip hitSound;
-    
+           
+    RaycastHit2D hit;
+    LayerMask mask;
+
+
+    float distance;
+    float originalDistance;
+    float distanceThisFrame;
+
     public override void Start()
     {
-        base.Start();
-
-        //if (ObjectGen == null)
-        //    ObjectGen = FindTag(gameObject);
+        base.Start();               
 
         startingPos = new Vector2(transform.position.x, transform.position.y);
 
         targetPos = PlayerThrow.Instance.touchPos;
         throwVector = (targetPos - startingPos).normalized * speed;
+
+        SetUpRay();
+        CheckForCollision();        
+
         Destroy(gameObject, 1);
-    }
-   
+               
+    }  
 
     public override void Update()
     {
-
+        currentPos = new Vector2(transform.position.x, transform.position.y);
+        CheckDistance();        
         setProjectilePos();
         setProjectileRotation();
+        SetScale();
 
         if (GameManager.Instance.gameOver == true)
         {
@@ -80,5 +93,51 @@ public class Newspaper : Projectile
             Destroy(gameObject);        
             
         }
-    }    
+    }
+
+    void CheckForCollision()
+    {
+        if (hit)
+        {
+            distance = hit.distance;
+        }
+        else
+            distance = 7;
+
+        originalDistance = distance;
+    }
+
+    void SetUpRay()
+    {
+        mask = LayerMask.GetMask("House");
+        targetDir = (targetPos - startingPos).normalized;
+
+        hit = Physics2D.Raycast(transform.position, targetDir, 10000f, mask);
+
+        Debug.DrawRay(transform.position, targetDir, Color.red, 1000f);
+    }
+
+    void CheckDistance()
+    {        
+        distanceVector = currentPos - startingPos;
+        distanceThisFrame = distanceVector.magnitude;
+        distance -= distanceThisFrame;
+        startingPos = currentPos;
+
+        Debug.Log(distance);
+    }
+
+    void SetScale()
+    {
+        if(distance > originalDistance/2)
+        {
+            gameObject.transform.localScale += new Vector3(0.01f, 0.01f, 0.0f);
+        }
+        else if (distance < originalDistance/2)
+        {
+            gameObject.transform.localScale -= new Vector3(0.01f, 0.01f, 0.0f);
+
+        }
+    }
+
 }
--- Assets/Scripts/Projectile/ProjectileArc.cs
+++ Assets/Scripts/Projectile/ProjectileArc.cs
@@ -1,27 +1,47 @@
 using UnityEngine;
 
-public class ProjectileArc : MonoBehaviour
-{
-    
-    RaycastHit2D hit;
-    // Start is called before the first frame update
-    void Start()
-    {
-        hit = Physics2D.Raycast(transform.position, Vector2.up, 10f);
+//public class ProjectileArc : MonoBehaviour
+//{
+//    Vector2 targetPos;
+//    Vector2 targetDir;
+//    Vector2 startPos;
+//    RaycastHit2D hit;
+//    LayerMask mask;
+
+//    float distance;
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
         
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(hit)
-        {
-            Debug.Log("Hit something: " + hit.collider.gameObject.name);
-        }
-    }
+//        CheckForCollision();
+//    }
 
-    void CheckForColliders()
-    {
-       
-    }
-}
+//    // Update is called once per frame
+//    void Update()
+//    {
+      
+//    }
+
+//    void CheckForCollision()
+//    {
+//        if (hit)
+//        {
+//            distance = hit.distance;
+//            Debug.Log("Hit something: " + hit.distance);
+//        }
+//        else
+//            distance = 7;
+//    }
+
+//    void SetUpRay()
+//    {        
+//        mask = LayerMask.GetMask("House");
+//        targetDir = (targetPos - startPos).normalized;
+
+//        hit = Physics2D.Raycast(transform.position, targetDir, 10000f, mask);
+
+//        Debug.DrawRay(transform.position, targetDir, Color.red, 1000f);
+//    }
+//}
